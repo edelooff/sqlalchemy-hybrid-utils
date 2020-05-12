a237ced045ce64ccb6d058ebf6fc6bccef36429b
Elmer de Looff <elmer.delooff@gmail.com>
2020-05-12 22:03:21 +0200

    Adds initial version of column-tracking boolean flag.

---
+++ sqlalchemy_column_flag.py
@@ -0,0 +1,24 @@
+from sqlalchemy.ext.hybrid import hybrid_property
+from sqlalchemy.inspection import inspect
+
+
+def column_flag(column, **options):
+    def target_key(instance):
+        target = inspect(instance).mapper.get_property_by_column(column)
+        return target.key
+
+    def fget(self):
+        return getattr(self, target_key(self)) is not None
+
+    def fset(self, value, default=options.get('default')):
+        if not isinstance(value, bool):
+            raise TypeError('Flag only accepts boolean values')
+        return setattr(self, target_key(self), default if value else None)
+
+    def expr(cls):
+        return column.isnot(None)
+
+    return hybrid_property(
+        fget=fget,
+        fset=fset if 'default' in options else None,
+        expr=expr)
