5f97ee8d0b1d5fd806e17639d94a45f326d74636
Elmer de Looff <elmer.delooff@gmail.com>
2020-05-21 16:43:22 +0200

    Adds DerivedColumn class for easier construction of hybric_property.

--- sqlalchemy_column_flag.py
+++ sqlalchemy_column_flag.py
@@ -2,23 +2,53 @@ from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy.inspection import inspect
 
 
+class DerivedColumn:
+    def __init__(self, column, default=None):
+        self.column = column
+        self.default = default
+
+    def _default_functions(self):
+        setter = self.default
+        if not callable(setter):
+            setter = lambda: self.default  # noqa
+        return {True: setter, False: lambda: None}
+
+    def make_getter(self):
+        def _fget(self, column=self.column):
+            mapper = inspect(self).mapper
+            target = mapper.get_property_by_column(column).key
+            return getattr(self, target) is not None
+
+        return _fget
+
+    def make_setter(self):
+        if self.default is None:
+            return None
+        defaults = self._default_functions()
+
+        def _fset(self, value, column=self.column, defaults=defaults):
+            if not isinstance(value, bool):
+                raise TypeError("Flag only accepts boolean values")
+            mapper = inspect(self).mapper
+            target = mapper.get_property_by_column(column).key
+            return setattr(self, target, defaults[value]())
+
+        return _fset
+
+    def make_expression(self):
+        def _expr(cls, expr=self.column.isnot(None)):
+            return expr
+
+        return _expr
+
+    def create_hybrid(self):
+        return hybrid_property(
+            fget=self.make_getter(),
+            fset=self.make_setter(),
+            expr=self.make_expression(),
+        )
+
+
 def column_flag(column, **options):
-    def target_key(instance):
-        target = inspect(instance).mapper.get_property_by_column(column)
-        return target.key
-
-    def fget(self):
-        return getattr(self, target_key(self)) is not None
-
-    def fset(self, value, default=options.get("default")):
-        if not isinstance(value, bool):
-            raise TypeError("Flag only accepts boolean values")
-        if callable(default):
-            default = default()
-        return setattr(self, target_key(self), default if value else None)
-
-    def expr(cls):
-        return column.isnot(None)
-
-    return hybrid_property(
-        fget=fget, fset=fset if "default" in options else None, expr=expr)
+    derived = DerivedColumn(column, **options)
+    return derived.create_hybrid()
