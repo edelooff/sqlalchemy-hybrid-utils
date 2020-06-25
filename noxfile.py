9df5d90a72dc89a00abd98c838d6d66831306c28
Elmer de Looff <elmer.delooff@gmail.com>
2020-06-25 22:47:12 +0200

    Folds black into flake session and renames all sessions to purpose rather than tool.

--- noxfile.py
+++ noxfile.py
@@ -2,27 +2,20 @@ import nox
 
 
 @nox.session(python="3.8")
-def black(session):
-    args = session.posargs or (".",)
-    session.install("black")
-    session.run("black", "--check", *args)
-
-
-@nox.session(python="3.8")
-def flake(session):
-    session.install("flake8")
+def lint(session):
+    session.install("flake8", "flake8-black")
     session.run("flake8", *session.posargs)
 
 
 @nox.session(python="3.8")
-def mypy(session):
+def type(session):
     session.install("mypy", "sqlalchemy-stubs")
     session.run("mypy", *session.posargs)
 
 
 @nox.session(python="3.8")
 @nox.parametrize("sqla", ["1.1", "1.2", "1.3"])
-def tests(session, sqla):
+def test(session, sqla):
     args = session.posargs or ["--cov"]
     session.run("poetry", "install", external=True)
     session.install(f"sqlalchemy=={sqla}")
