a1f3d6afe5eba77caea49ea925d6098dca6ffe92
Elmer de Looff <elmer.delooff@gmail.com>
2024-02-25 17:00:25 +0100

    Removes specific Python versions from noxfile.

--- noxfile.py
+++ noxfile.py
@@ -1,19 +1,19 @@
 import nox
 
 
-@nox.session(python="3.10")
+@nox.session
 def lint(session):
     session.install("flake8", "flake8-black", "flake8-bugbear", "flake8-isort")
     session.run("flake8", "src", "tests", *session.posargs)
 
 
-@nox.session(python="3.10")
+@nox.session
 def type(session):
     session.install("mypy", "sqlalchemy-stubs")
     session.run("mypy", *session.posargs)
 
 
-@nox.session(python="3.10")
+@nox.session
 @nox.parametrize("sqlalchemy", ["1.3", "1.4", "2.0"])
 def test(session, sqlalchemy):
     args = session.posargs or ["--cov"]
